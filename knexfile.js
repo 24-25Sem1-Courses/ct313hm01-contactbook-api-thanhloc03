155eb0400e607de23d86621c69827d844cd8ac17
Ngo Thanh Loc <ngothanhloc.22102003@gmail.com>
2024-09-18 08:04:00 +0700

    Setup knex.js and insert fake data

---
+++ knexfile.js
@@ -0,0 +1,29 @@
+// Update with your config settings.
+
+/*global module require process*/
+require("dotenv").config();
+const { DB_HOST, DB_PORT, DB_USER, DB_NAME } = process.env;
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+    client: 'mysql',
+    connection: {
+      host: DB_HOST,
+      port: DB_PORT,
+      user: DB_USER,
+      // password: DB_PASS,
+      database: DB_NAME,
+    },
+
+    pool: {
+      min: 0,
+      max: 10
+    },
+
+    seeds: {
+      directory: './seeds',
+    },
+};
